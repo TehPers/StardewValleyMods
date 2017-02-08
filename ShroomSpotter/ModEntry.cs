61921f35202765d5de945efc253f634190128392
TehPers <admin@tehpers.com>
2017-02-08 00:42:16 -0600

    Shroom days and locations now appear in the calendar.

--- ShroomSpotter/ModEntry.cs
+++ ShroomSpotter/ModEntry.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Xna.Framework.Input;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Locations;
+using StardewValley.Menus;
 using System;
 using System.Collections.Generic;
 
@@ -26,6 +29,9 @@ namespace TehPers.Stardew.ShroomSpotter {
             //this.Monitor.Log("It is *HIGHLY* recommended you install a Health Bars mod for enemies!", LogLevel.Info);
 
             ControlEvents.KeyPressed += KeyPressed;
+            GraphicsEvents.OnPreRenderGuiEvent += OnPreRenderGuiEvent;
+            GraphicsEvents.OnPostRenderGuiEvent += OnPostRenderGuiEvent;
+            //MenuEvents.MenuChanged += MenuEvents_MenuChanged;
         }
 
         #region Events
@@ -33,18 +39,9 @@ namespace TehPers.Stardew.ShroomSpotter {
             Keys getShroomLevels;
             if (Enum.TryParse(config.GetShroomLevels, out getShroomLevels) && e.KeyPressed == getShroomLevels) {
                 // Find all shroom levels
-                int daysTilShroom = -1;
                 List<int> shroomLevels = new List<int>();
-                while (shroomLevels.Count == 0 && ++daysTilShroom < 50)
-                for (int mineLevel = 1; mineLevel < 120; mineLevel++) {
-                    Random random = new Random((int) Game1.stats.DaysPlayed + daysTilShroom + mineLevel + (int) Game1.uniqueIDForThisGame / 2);
-
-                    // Simulate all the random values grabbed before the shrooms
-                    if (random.NextDouble() < 0.3 && mineLevel > 2) random.NextDouble();
-                    random.NextDouble();
-                    if (random.NextDouble() < 0.035 && mineLevel >= 80 && mineLevel <= 120 && mineLevel % 5 != 0)
-                        shroomLevels.Add(mineLevel);
-                }
+                int daysTilShroom = -1;
+                while (shroomLevels.Count == 0 && ++daysTilShroom < 50) shroomLevels = this.getShroomLayers(daysTilShroom);
 
                 if (shroomLevels.Count > 0) {
                     if (daysTilShroom == 0)
@@ -54,6 +51,73 @@ namespace TehPers.Stardew.ShroomSpotter {
                 } else Game1.showGlobalMessage("No shroom layers will spawn in the next 50 days!");
             }
         }
+
+        private void OnPreRenderGuiEvent(object sender, EventArgs e) {
+            if (Game1.activeClickableMenu is Billboard) {
+                Billboard menu = (Billboard) Game1.activeClickableMenu;
+                List<ClickableTextureComponent> calendarDays = this.Helper.Reflection.GetPrivateValue<List<ClickableTextureComponent>>(menu, "calendarDays");
+                IPrivateField<string> hoverField = this.Helper.Reflection.GetPrivateField<string>(menu, "hoverText");
+                string hoverText = hoverField.GetValue();
+                if (!(hoverText.Contains("Shrooms") || hoverText.Contains("shrooms"))) {
+                    for (int day = 1; day <= 28; day++) {
+                        ClickableTextureComponent component = calendarDays[day - 1];
+                        if (component.bounds.Contains(Game1.getMouseX(), Game1.getMouseY())) {
+                            List<int> shrooms = this.getShroomLayers(day - Game1.dayOfMonth);
+
+                            if (hoverText.Length > 0)
+                                hoverText += "\n";
+
+                            if (shrooms.Count > 0)
+                                hoverText += "Shrooms: " + string.Join(", ", shrooms);
+                            else
+                                hoverText += "No shrooms";
+
+                            break;
+                        }
+                    }
+
+                    hoverField.SetValue(hoverText);
+                }
+            }
+        }
+
+        private void OnPostRenderGuiEvent(object sender, EventArgs e) {
+            if (Game1.activeClickableMenu is Billboard) {
+                Billboard menu = (Billboard) Game1.activeClickableMenu;
+                List<ClickableTextureComponent> calendarDays = this.Helper.Reflection.GetPrivateValue<List<ClickableTextureComponent>>(menu, "calendarDays");
+                string hoverText = this.Helper.Reflection.GetPrivateValue<string>(menu, "hoverText");
+                SpriteBatch b = Game1.spriteBatch;
+
+                for (int day = 1; day <= 28; day++) {
+                    ClickableTextureComponent component = calendarDays[day - 1];
+                        List<int> shrooms = this.getShroomLayers(day - Game1.dayOfMonth);
+
+                    if (shrooms.Count > 0) {
+                        const int id = 422;
+                        Rectangle source = Game1.currentLocation.getSourceRectForObject(id);
+                        Vector2 dest = new Vector2(component.bounds.X, component.bounds.Y + 10f * Game1.pixelZoom);
+                        b.Draw(Game1.objectSpriteSheet, dest, new Rectangle?(source), Color.White, 0.0f, Vector2.Zero, Game1.pixelZoom / 2f, SpriteEffects.None, 1f);
+                    }
+                }
+
+                IClickableMenu.drawHoverText(b, hoverText, Game1.dialogueFont, 0, 0, -1, null, -1, null, null, 0, -1, -1, -1, -1, 1f, null);
+            }
+        }
         #endregion
+
+        public List<int> getShroomLayers(int relativeDay) {
+            List<int> shroomLevels = new List<int>();
+            for (int mineLevel = 1; mineLevel < 120; mineLevel++) {
+                Random random = new Random((int) Game1.stats.DaysPlayed + relativeDay + mineLevel + (int) Game1.uniqueIDForThisGame / 2);
+
+                // Simulate all the random values grabbed before the shrooms
+                if (random.NextDouble() < 0.3 && mineLevel > 2) random.NextDouble();
+                random.NextDouble();
+                if (random.NextDouble() < 0.035 && mineLevel >= 80 && mineLevel <= 120 && mineLevel % 5 != 0)
+                    shroomLevels.Add(mineLevel);
+            }
+
+            return shroomLevels;
+        }
     }
 }
