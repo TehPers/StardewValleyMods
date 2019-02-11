9f5cb5c4568c6589b3487ccf47a2acfaf21e7bfb
TehPers <tehperz@gmail.com>
2019-02-10 22:40:49 -0600

    Cleaned up repository Fixed crafting recipes not working

--- TehPers.CoreMod.Api/System/Range.cs
+++ TehPers.CoreMod.Api/System/Range.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace TehPers.CoreMod.Api.Conflux.Collections {
-    public readonly struct Range : IEnumerable<int> {
+namespace System {
+    public readonly struct Range : IEnumerable<int>, IEquatable<Range> {
         /// <summary>The start of this range.</summary>
         public Index Start { get; }
 
@@ -30,22 +31,20 @@ namespace TehPers.CoreMod.Api.Conflux.Collections {
         /// <summary>Creates a new range starting from an index and extending to the end.</summary>
         /// <param name="start">The starting index.</param>
         /// <returns>A range which spans from the specified start to the end.</returns>
-        public static Range From(Index start) {
+        public static Range StartAt(Index start) {
             return new Range(start, new Index(0, true));
         }
 
         /// <summary>Creates a new range starting from the beginning and extending to the specified index.</summary>
         /// <param name="end">The end index of the range.</param>
         /// <returns>A range which spans from the start to the specified index.</returns>
-        public static Range To(Index end) {
+        public static Range EndAt(Index end) {
             return new Range(0, end);
         }
 
         /// <summary>Creates a new range starting from the beginning and extending to the end.</summary>
         /// <returns>A range which covers all possible indexes.</returns>
-        public static Range All() {
-            return new Range(0, new Index(0, true));
-        }
+        public static Range All => new Range(Index.Start, Index.End);
 
         public static implicit operator Range((int Start, int End) range) {
             return new Range(range.Start, range.End);
@@ -53,16 +52,41 @@ namespace TehPers.CoreMod.Api.Conflux.Collections {
 
         /// <inheritdoc />
         public IEnumerator<int> GetEnumerator() {
-            int start = this.Start.Value * (this.Start.FromEnd ? -1 : 1);
-            int end = this.End.Value * (this.End.FromEnd ? -1 : 1);
-            for (int n = start; n <= end; n++) {
-                yield return n;
-            }
+            int start = this.Start.Value * (this.Start.IsFromEnd ? -1 : 1);
+            int end = this.End.Value * (this.End.IsFromEnd ? -1 : 1);
+            return Enumerable.Range(start, end - start).GetEnumerator();
         }
 
         /// <inheritdoc />
         IEnumerator IEnumerable.GetEnumerator() {
             return this.GetEnumerator();
+        }
+
+        public bool Equals(Range other) {
+            return this.Start.Equals(other.Start) && this.End.Equals(other.End);
+        }
+
+        public override int GetHashCode() {
+            return unchecked(this.Start.GetHashCode() ^ 397 + this.End.GetHashCode());
+        }
+
+        public override string ToString() {
+            return $"{this.Start}..{this.End}";
+        }
+
+        public readonly struct OffsetAndLength {
+            public int Offset { get; }
+            public int Length { get; }
+
+            public OffsetAndLength(int offset, int length) {
+                Offset = offset;
+                Length = length;
+            }
+
+            public void Deconstruct(out int offset, out int length) {
+                offset = Offset;
+                length = Length;
+            }
         }
     }
 }
